fafd0008c0ccdb5611dd267c63458c7bbb422aa6
David R. Smith <drsmith8@wisc.edu>
2021-12-02 15:20:55 -0500

    mods

--- detectors.py
+++ detectors.py
@@ -10,14 +10,16 @@ from pathlib import Path
 import numpy as np
 import scipy.constants as pc
 
-#  CPD = current power density [A^2/Hz]
-#  VPD = voltage power density [V^2/Hz]
+
+#  Terminology
+#  CNPD = current noise power density [A^2/Hz]
+
 
 class _Diode(object):
 
-    #  Diode noise sources
-    #  CPD: Shot noise from diode photocurrent and dark currents
-    #  CPD: Thermal (Johnson) noise from diode shunt resistance
+    #  Diode current noise sources:
+    #  Shot noise from diode photocurrent and dark currents
+    #  Thermal (Johnson) noise from diode shunt resistance
     
     p_ref = 10  # reference incident power [nW]
     t_ref = 20  # reference temp [C]
@@ -26,36 +28,30 @@ class _Diode(object):
                  name='', 
                  responsivity=0,  # responsivity [V/A]
                  qe=0,  # quantum efficiancy [0-1]
-                 darkcurrent=10e-9,  # dark (leakage) current (or volumetric if APD) [A]
-                 darkcurrent_surface=0,  # dark (leakage) surface current for APD [A]
-                 junction_cap=10e-15,  # diode junction cap. [F]
-                 r_shunt=300e6,  # shunt resistance [Ohms]
+                 darkcurrent_ref=10e-9,  # dark (leakage) current (or volumetric if APD) [A]
+                 darkcurrent_surface_ref=0,  # dark (leakage) surface current for APD [A]
+                 junction_cap_ref=10e-15,  # diode junction cap. [F]
                  vb_ref=10,  # reference reverse bias [V]
+                 r_shunt=500e6,  # shunt resistance [Ohms]
                  ):
+        assert(qe>0 or responsivity>0)
         self.name = name
         self.gain = None
         self.noise_factor = None
         if responsivity:
             # responsivity at 650 nm [A/W]
+            if qe:
+                print('Warning: Using input responsivity, ignoring input QE')
             self.responsivity = responsivity
             self.qe = self.responsivity * 1240/656
         elif qe:
             self.qe = qe
             self.responsivity = self.qe * 656/1240
-        assert(self.qe>0 or self.responsivity>0)
-        # dark current at V_b=10V, T=20C [nA]
-        # drops by half for 20 C decrease
-        # slight increase with V_b
-        assert(darkcurrent>0)
-        self.darkcurrent_ref = darkcurrent  # total or volumetric, by PIN/APD context
-        self.darkcurrent_surface_ref = darkcurrent_surface  # 0 if n/a
-        # junction cap at V_b=10V, T=20C [pF]
-        # sharp decrease with V_b; C_j ~ 1/sqrt(V_b)
-        # C_j(10V) ~ C_j(0V)/5
-        assert(junction_cap>0)
-        self.junction_cap_ref = junction_cap
-        self.r_shunt = r_shunt
+        self.darkcurrent_ref = darkcurrent_ref  # total or volumetric, by PIN/APD context
+        self.darkcurrent_surface_ref = darkcurrent_surface_ref  # 0 if n/a
+        self.junction_cap_ref = junction_cap_ref
         self.vb_ref = vb_ref
+        self.r_shunt = r_shunt
         print(self.name)
         print(f'  QE = {self.qe*1e2:.1f} %')
         print(f'  Resp. = {self.responsivity:.2f} A/W')
@@ -77,52 +73,35 @@ class _Diode(object):
             junction_cap *= 0
         return junction_cap
     
-    def shot_noise_CPD(self, p_inc=p_ref, t=t_ref, ideal=False):
-        #  Shot current noise power density [A^2/Hz]
+    def photocurrent_shot_noise_CNPD(self, p_inc=p_ref, ideal=False):
         photocurrent = self.photocurrent(p_inc=p_inc)
+        noise_factor = self.noise_factor
+        if ideal:
+            noise_factor = 1
+        shot_noise = 2*pc.e * photocurrent * self.gain**2 * noise_factor 
+        return shot_noise
+
+    def dark_current_shot_noise_CNPD(self, t=t_ref, ideal=False):
         # decrease by half for every 10C below ref. temp.
         darkcurrent_temp_factor = 2**((t-self.t_ref)/10)
         # dark current or volumetric dark current depending on PIN/APD context
         darkcurrent = self.darkcurrent_ref * darkcurrent_temp_factor
-        # surface dark current, if applicable
         darkcurrent_surface = self.darkcurrent_surface_ref * darkcurrent_temp_factor
         noise_factor = self.noise_factor
         if ideal:
             darkcurrent *= 0
             darkcurrent_surface *= 0
             noise_factor = 1
-        shot_noise = 2*pc.e * ( \
-            (photocurrent + darkcurrent) * self.gain**2 * noise_factor + \
-            darkcurrent_surface)
+        shot_noise = 2*pc.e * ( darkcurrent * self.gain**2 * noise_factor + \
+            darkcurrent_surface )
         return shot_noise
 
-    def shunt_noise_CPD(self, t=t_ref, ideal=False):
-        #  Shunt thermal current noise power density [A^2/Hz]
+    def shunt_noise_CNPD(self, t=t_ref, ideal=False):
         shunt_noise = 4*pc.k*(273+t) / self.r_shunt
         if ideal:
             shunt_noise *= 0
         return shunt_noise
 
-    # def shot_noise_current(self, p_inc=p_ref, t=t_ref, bw=bw_ref):
-    #     shot_noise_current = np.sqrt(self.shot_noise_power_density(p_inc=p_inc, t=t) * bw)
-    #     return shot_noise_current
-
-    # def shunt_noise_current(self, r_shunt=r_shunt_ref, t=t_ref, bw=bw_ref):
-    #     shunt_noise_current = np.sqrt(self.shunt_noise_power_density(r_shunt=r_shunt, t=t) * bw)
-    #     return shunt_noise_current
-
-    # def total_noise_current(self, p_inc=p_ref, t=t_ref, r_shunt=r_shunt_ref, bw=bw_ref):
-    #     shot_noise_power_density = self.shot_noise_power_density(p_inc=p_inc, t=t)
-    #     shunt_noise_power_density = self.shunt_noise_power_density(r_shunt=r_shunt, t=t)
-    #     total_noise_power_density = shot_noise_power_density + shunt_noise_power_density
-    #     total_noise_current = np.sqrt(total_noise_power_density * bw)
-    #     return total_noise_current
-    
-    # def SNR(self, p_inc=p_ref, t=t_ref, bw=bw_ref, r_shunt=r_shunt_ref):
-    #     photocurrent = self.photocurrent(p_inc=p_inc)
-    #     total_noise_current = self.total_noise_current(p_inc=p_inc, t=t, bw=bw, r_shunt=r_shunt)
-    #     return photocurrent / total_noise_current
-    
 
 class PinDiode(_Diode):
     
@@ -154,47 +133,21 @@ class ApdDiode(_Diode):
         elif noise_current:
             print(f'  Noise current = {noise_current:.3g}')
             # calc noise factor from noise current
-            sigma_sq_ideal = self.shot_noise_CPD(p_inc=0)/self.bw_ref
+            sigma_sq_ideal = self.shot_noise_CNPD(p_inc=0)/self.bw_ref
             self.noise_factor = noise_current**2 / sigma_sq_ideal
         assert(self.noise_factor>1)
         print(f'  Noise factor = {self.noise_factor:.3f}')
 
 
-diodes = {
-    'API_C164_PIN' : PinDiode(
-        name='API PDB-C164 disc. PIN',
-        responsivity=0.43,  # A/W @ 650 nm
-        darkcurrent=1e-9,  # typ 1 nA, max 10 nA @ T=20C
-        junction_cap=7e-12,  # typ 7 pF @ Vr=10V
-    ),
-    'Hama_S13620_PIN' : PinDiode(
-        name='Hama S13620 8x8 PIN',
-        responsivity=0.47,  # A/W @ 650 nm
-        darkcurrent=0.3e-9,  # typ 10 pA, max 300 pA @ Vr=10mV, T=20C
-        junction_cap=60e-12,  # typ 60 pF @ Vr=0V
-    ),
-    'Hama_S8550_APD' : ApdDiode(
-        name='Hama S8550 4x8 APD',
-        qe=0.85,  # QE @ 650 nm
-        gain=50,  # gain @ Vr=330 V
-        darkcurrent=1e-9,  # typ 1 nA, max 10 nA @ Vr=330V, M=50
-        junction_cap=9e-12,  # typ 9 pF @ Vr=330V, M=50
-        noise_figure=0.2,
-    ),
-}
-
-
 class TIA(object):
 
-    #  Noise sources:
+    #  TIA current noise sources:
     #  Shot noise from JFET gate current
     #  Thermal (Johnson) noise from TIA feedback resistor
     #  JFET channel voltage noise and "ENC" current noise
     
-    t_ref = 20  # reference temp [C]
-    p_ref = 10  # reference incident power [nW]
-
-    # bw_ref = 1e6  # reference bandwidth [Hz]
+    t_ref = _Diode.t_ref  # reference temp [C]
+    f_ref = np.logspace(1e3, 1e6)
 
     def __init__(self,
             diode=None,
@@ -202,7 +155,7 @@ class TIA(object):
             jfet_enoise=0.8e-9,  # reference JFET e-nosie [V/root(Hz)], typ. 1 nV/rt(Hz)
             jfet_gatecurrent=1e-9,  # reference JFET gate current [A], typ. 10 mA
             jfet_input_cap=10e-15,  # reference JFET input cap. [F]
-            stray_cap=10e-15,   # PCB stray capacitance [F]
+            stray_cap=4e-15,   # PCB stray capacitance [F]
             ):
         assert(diode and isinstance(diode, _Diode))
         self.diode = diode
@@ -212,30 +165,56 @@ class TIA(object):
         self.jfet_input_cap = jfet_input_cap
         self.stray_cap = stray_cap
 
-    def feedback_noise_CPD(self, t=t_ref, r_feedback=None, ideal=False):
+    def feedback_noise_CNPD(self, t=t_ref, r_feedback=None, ideal=False):
         if not r_feedback:
             r_feedback = self.r_feedback
-        #  current noise power density [A^2/Hz]
         feedback_noise = 4*pc.k*(273+t) / r_feedback
         if ideal:
             feedback_noise *= 0
         return feedback_noise
 
-    def gate_noise_CPD(self, jfet_gatecurrent=None, ideal=False):
+    def gate_noise_CNPD(self, jfet_gatecurrent=None, ideal=False):
         if not jfet_gatecurrent:
             jfet_gatecurrent = self.jfet_gatecurrent
-        #  current noise power density [A^2/Hz]
         gate_noise = 2*pc.e * jfet_gatecurrent
         if ideal:
             gate_noise *= 0
         return gate_noise
 
-    def enc_noise_CPD(self, vb=None, ideal=False):
-        #  current noise power density [A^2/Hz]
-        diode_junction_cap = self.diode.junction_cap(vb=vb, ideal=ideal)
+    def enc_noise_CNPD(self, f=f_ref, diode_junction_cap=0, vb=None, ideal=False):
+        if vb:
+            print('Using V_b to calculate diode junction cap.')
+            diode_junction_cap = self.diode.junction_cap(vb=vb, ideal=ideal)
         total_cap = diode_junction_cap + self.jfet_input_cap + self.stray_cap
-        enc_noise = 0
+        enc_noise = ( 2*np.pi * self.jfet_enoise * total_cap * f ) ** 2.0
         if ideal:
             enc_noise *= 0
         return enc_noise
 
+
+# diodes = {
+#     'API_C164_PIN' : PinDiode(
+#         name='API PDB-C164 disc. PIN',
+#         responsivity=0.43,  # A/W @ 650 nm
+#         darkcurrent_ref=1e-9,  # typ 1 nA, max 10 nA @ T=20C
+#         junction_cap_ref=7e-12,  # typ 7 pF @ Vr=10V
+#         vb_ref=10,  # reference bias voltage for junction cap.
+#         r_shunt=500e6,  # shunt resistance
+#     ),
+#     'Hama_S13620_PIN' : PinDiode(
+#         name='Hama. S13620 8x8 PIN',
+#         responsivity=0.49,  # A/W @ 650 nm
+#         darkcurrent_ref=0.3e-9,  # typ 10 pA, max 300 pA @ Vr=10mV, T=20C
+#         junction_cap_ref=15e-12,  # typ 15 pF @ Vr=8.6V
+#         vb_ref=8.6,
+#     ),
+#     'Hama_S8550_APD' : ApdDiode(
+#         name='Hama. S8550 4x8 APD',
+#         qe=0.85,  # QE @ 650 nm
+#         gain=50,  # gain @ Vr=330 V
+#         darkcurrent_ref=1e-9,  # typ 1 nA, max 10 nA @ Vr=330V, M=50
+#         junction_cap_ref=9e-12,  # typ 9 pF @ Vr=330V, M=50
+#         noise_figure=0.2,
+#         vb_ref=330,  # reference bias for gain and junction cap.
+#     ),
+# }
